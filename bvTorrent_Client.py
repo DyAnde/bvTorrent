1c25bb9ec584a0e6e0ab5b7fe7a47d915385e175
DyAnde <21dyande@gmail.com>
2025-02-17 15:00:09 -0600

    Added verbose comments for functions

--- bvTorrent_Client.py
+++ bvTorrent_Client.py
@@ -1,32 +1,56 @@
-
-import socket
+from threading import *
+from socket import *
 
 
 # client to tracker
 
 # in a try block:
-    # recieve filename
-    # recieve chunksize
-    # recieve numchunks
+	# recieve filename
+	# recieve chunksize
+	# recieve numchunks
 
-    # for numchunks send the hashed data (chunksize)
-    # send back to the tracker the port it is listening on
+	# for numchunks send the hashed data (chunksize)
+	# send back to the tracker the port it is listening on
+
+# New connection to the swarm.
+# - Client will recieve file and chunk info from tracker
+# - Client will tell tracker:
+# 	- Which port it will listen on to recieve incoming connections
+# 	- It's current chunk mask (denoting which chunks it has)
+def newConnection():
+	pass
 
 # have the functions outlined in class 2/13
 
+# Update the mask of the client
+# - Client tells tracker which chunks it has downloaded
 def updateMask():
-    pass
+	pass
 
+# Client requests an updated list of all other clients in the swarm and their chunk masks
 def clientListReq():
-    pass
+	pass
 
 def getClientlist():
-    pass
+	pass
 
+# Cleanly disconnect from the swarm
 def Disconnect():
-    pass
+	pass
 
-    # Client to Client
-        # send the chunk index you would like to recieve
+# Client to Client
+	# send the chunk index you would like to recieve
 
-        #recieve binary chunk data
+	#recieve binary chunk data
+# Client requests a connection to another client
+# ** ONE SOCKET PER CHUNK **
+# Protocol:
+# 	- Requesting client establishes connection with another client that has the desired chunk(s)
+# 	- Requesting client will send single integer in an ASCII string that is new line terminated.
+# 		- Integer represents the chunk id (0-based)
+# 	- Client with the chunk will send back a byte array containing the number of bytes in the chunk
+# 		- No new line termination
+# 	- Recieving client will hash the recieved data to derive its checksum and confirm the derived checksum matches the checksum matches the checksum the tracker provided
+def client_to_client():
+	pass
+
