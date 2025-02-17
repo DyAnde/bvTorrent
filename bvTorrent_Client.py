564b9e23bec4f59475e01380c67e6fe95d47e3b4
DyAnde <21dyande@gmail.com>
2025-02-17 15:56:20 -0600

    converted function comments to docstrings

--- bvTorrent_Client.py
+++ bvTorrent_Client.py
@@ -3,18 +3,22 @@ from socket import *
 from sys import argv
 from pathlib import *
 
-# New connection to the swarm.
-# - Client will recieve file and chunk info from tracker
-# - Client will tell tracker:
-# 	- Which port it will listen on to recieve incoming connections
-# 	- It's current chunk mask (denoting which chunks it has)
-def newConnection():
+# doing something like "print(updateMask.__doc__)" will print the docstring of the function
+
 # in a try block:
 	# recieve filename
 	# recieve chunksize
 	# recieve numchunks
 	# for numchunks send the hashed data (chunksize)
 	# send back to the tracker the port it is listening on
+def newConnection():
+	"""
+	New connection to the swarm.
+	 - Client will recieve file and chunk info from tracker
+	 - Client will tell tracker:
+		- Which port it will listen on to recieve incoming connections
+		- It's current chunk mask (denoting which chunks it has)
+	"""
 	pass
 
 
@@ -28,32 +32,38 @@ elif len(argv) != 1:
 	exit()
 
 
-# Update the mask of the client
-# - Client tells tracker which chunks it has downloaded
 def updateMask():
+	"""
+	Update the mask of the client
+	- Client tells tracker which chunks it has downloaded
+	"""
 	pass
 
-# Client requests an updated list of all other clients in the swarm and their chunk masks
+
 def clientListReq():
+	"""Client requests an updated list of all other clients in the swarm and their chunk masks"""
 	pass
+
 
 def getClientlist():
 	pass
 
-# Cleanly disconnect from the swarm
+
 def Disconnect():
+	"""Cleanly disconnect from the swarm"""
 	pass
 
 
-# Client requests a connection to another client
 # ** ONE SOCKET PER CHUNK e.g. REQUEST SINGLE CHUNK, CLOSE CONNECTION, OPEN NEW CONNECTION, REQUEST SINGLE CHUNK, etc.**
-# Protocol:
-# 	- Requesting client establishes connection with another client that has the desired chunk(s)
-# 	- Requesting client will send single integer in an ASCII string that is new line terminated.
-# 		- Integer represents the chunk id (0-based)
-# 	- Client with the chunk will send back a byte array containing the number of bytes in the chunk
-# 		- No new line termination
-# 	- Recieving client will hash the recieved data to derive its checksum and confirm the derived checksum matches the checksum matches the checksum the tracker provided
 def client_to_client():
+	"""
+	Client requests a connection to another client
+	Protocol:
+		- Requesting client establishes connection with another client that has the desired chunk(s)
+		- Requesting client will send single integer in an ASCII string that is new line terminated.
+			- Integer represents the chunk id (0-based)
+		- Client with the chunk will send back a byte array containing the number of bytes in the chunk
+			- No new line termination
+		- Recieving client will hash the recieved data to derive its checksum and confirm the derived checksum matches the checksum matches the checksum the tracker provided
+	"""
 	pass
-
